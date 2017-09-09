aff749a82cd979abd2e7b3fd63d0a0ecbd0f5b2c
ebricco <ewbricco>
2017-09-09 13:37:56 -0500

    adding readme

--- src/eastin/main/java/Main.java
+++ src/eastin/main/java/Main.java
@@ -16,7 +16,7 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
 
-        //surpress annoying htmlunit console output
+        //suppress annoying htmlunit console output
         LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
         java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
         java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
